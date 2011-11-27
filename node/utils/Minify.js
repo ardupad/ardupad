a162b62c9f792d2541b5d256f7fc3bdeb13f1dbc
Pierros Papadeas <ppapadeas@gmail.com>
2011-11-27 18:36:55 +0200

    new text placeholder

--- node/utils/Minify.js
+++ node/utils/Minify.js
@@ -4,7 +4,7 @@
  */ 
 
 /*
- * 2011 Peter 'Pita' Martischka (Primary Technology Ltd)
+ * 2011 Peter 'Pita' Martischka (Primary Technology Ltd), Pierros Papadeas
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
